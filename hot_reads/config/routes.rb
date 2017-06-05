45d124244f98ecdba1aeca5a44fd266a395967f3
Ryan Spink <ryanspinktech@gmail.com>
2017-06-05 12:01:13 -0600

    passing toplinks test

--- hot_reads/config/routes.rb
+++ hot_reads/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "toplinks#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "*all" => "application#cors_preflight_check", :constraints => { :method => "OPTIONS" }
